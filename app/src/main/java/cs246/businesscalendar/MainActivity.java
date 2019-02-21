faa71e9577fb557934b4e9890998b28b6b6425ed
mb1 <bay15003@byui.edu>
2019-02-20 19:04:44 -0700

    Edited same line for Team Activity

--- app/src/main/java/cs246/businesscalendar/MainActivity.java
+++ app/src/main/java/cs246/businesscalendar/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //added comment for team activity for Jordan (take 2)
+        //added comment for team activity for Jordan (take 3) - MB Edited
         // MB - Comment Added for Team Activity
     }
 }
