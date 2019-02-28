e5e8a12a9c08bb8fa3fabb8dfd879a89418951b7
starktj7 <starktj7@byui.edu>
2019-02-27 18:29:16 -0800

    junit test

--- app/src/test/java/cs246/businesscalendar/ExampleUnitTest.java
+++ app/src/test/java/cs246/businesscalendar/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package cs246.businesscalendar;
 
-import org.junit.Test;
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
