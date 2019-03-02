ddc0f8df9ef9080be2fb7affdd4148271f62503b
mb1 <bay15003@byui.edu>
2019-03-02 11:26:49 -0700

    Created View for Welcome Screen and Added Blank View for Create Account Screen.  Also removed initial @Test items

--- app/src/main/java/cs246/businesscalendar/Welcome.java
+++ app/src/main/java/cs246/businesscalendar/Welcome.java
@@ -3,12 +3,12 @@ package cs246.businesscalendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
         //added comment for team activity for Jordan
         // MB - Comment Added for Team Activity - My Edit
         // MB - Comment Added for Team Activity- edited by JJ
