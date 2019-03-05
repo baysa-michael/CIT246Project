84b9bb4823f18c8802ea08363f9fba0e6551f53d
mb1 <bay15003@byui.edu>
2019-03-04 19:55:01 -0700

    Alignment Update to Create Account View Created Login View

--- app/src/main/java/cs246/businesscalendar/CreateAccount.java
+++ app/src/main/java/cs246/businesscalendar/CreateAccount.java
@@ -1,13 +1,37 @@
 package cs246.businesscalendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CreateAccount extends AppCompatActivity {
+    Spinner timeZoneSpinner;
+    Spinner time1224HSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        // Set Drop-down Lists
+        timeZoneSpinner = (Spinner) findViewById(R.id.createaccountTimeZoneSpinner);
+        time1224HSpinner = (Spinner) findViewById(R.id.createaccount1224HSpinner);
+
+        // Create Array Adapters
+        ArrayAdapter<CharSequence> timeZoneAdapter = ArrayAdapter.createFromResource(this,
+                R.array.time_zone, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> time1224HAdapter = ArrayAdapter.createFromResource(this,
+                R.array.time_1224, android.R.layout.simple_spinner_item);
+
+        // Specify layout for spinner dropdown items when list appears
+        timeZoneAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        time1224HAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the appropriate spinner
+        timeZoneSpinner.setAdapter(timeZoneAdapter);
+        time1224HSpinner.setAdapter(time1224HAdapter);
     }
+
 }
--- app/src/main/java/cs246/businesscalendar/Welcome.java
+++ app/src/main/java/cs246/businesscalendar/Welcome.java
@@ -1,7 +1,9 @@
 package cs246.businesscalendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -13,5 +15,15 @@ public class Welcome extends AppCompatActivity {
         // MB - Comment Added for Team Activity - My Edit
         // MB - Comment Added for Team Activity- edited by JJ
         // TS = comment for Team Activity
+    }
+
+    /**
+     * Called when user taps on Create New Account Button
+     */
+    public void clickCreateNewAccount(View view) {
+        // Create and Start Intent to Create New Account Activity
+        Intent thisIntent = new Intent(this, CreateAccount.class);
+
+        startActivity(thisIntent);
     }
 }
