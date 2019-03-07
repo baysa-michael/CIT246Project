422cd4aa8810d5ca89d7234353d64871f97b9983
mb1 <bay15003@byui.edu>
2019-03-06 18:42:16 -0700

    Built intent from Login to Landing Views

--- app/src/main/java/cs246/businesscalendar/Login.java
+++ app/src/main/java/cs246/businesscalendar/Login.java
@@ -1,7 +1,9 @@
 package cs246.businesscalendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void clickLogin(View view) {
+        // Create and Start Intent to Landing Page
+        // ******THIS WILL NEED TO BE DELETED AND CHANGED WITH AN ACTUAL LOGIN PROCESS
+        Intent thisIntent = new Intent(this, Landing.class);
+
+        startActivity(thisIntent);
     }
 }
