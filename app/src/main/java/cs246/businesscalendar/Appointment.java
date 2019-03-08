16dd52d2ea4bce800aaca61ab1ba11c30ac64a88
mb1 <bay15003@byui.edu>
2019-03-07 19:19:48 -0700

    Added Appointment View and added intent from Add New View to Appointment View.  Updated strings.xml and dimensions.xml.

--- app/src/main/java/cs246/businesscalendar/Appointment.java
+++ app/src/main/java/cs246/businesscalendar/Appointment.java
@@ -1,22 +1,15 @@
 package cs246.businesscalendar;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class AddNew extends AppCompatActivity {
+public class Appointment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_new);
-    }
-
-    public void clickAddNewAppointment(View view) {
-        Intent thisIntent = new Intent(this, Appointment.class);
-
-        startActivity(thisIntent);
+        setContentView(R.layout.activity_appointment);
     }
 
     public void clickCancel(View view) {
