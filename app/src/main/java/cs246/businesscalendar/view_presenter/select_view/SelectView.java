6ab4b36c87ef65785ef44025d8952082fae80641
mb1 <bay15003@byui.edu>
2019-03-16 12:17:20 -0600

    Modified activity movement logic to implement MVP for SelectView Package

--- app/src/main/java/cs246/businesscalendar/view_presenter/select_view/SelectView.java
+++ app/src/main/java/cs246/businesscalendar/view_presenter/select_view/SelectView.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import cs246.businesscalendar.R;
 
@@ -12,45 +13,71 @@ import cs246.businesscalendar.view_presenter.monthly_calendar.MonthlyCalendar;
 import cs246.businesscalendar.view_presenter.weekly_calendar.WeeklyCalendar;
 
 public class SelectView extends AppCompatActivity implements SelectViewContract.View {
+    private static final String TAG = "SelectView";
+    private Button monthlyViewButton;
+    private Button weeklyViewButton;
+    private Button dailyViewButton;
+    private Button cancelButton;
+    private SelectViewPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_view);
+
+        // Set Buttons
+        monthlyViewButton = findViewById(R.id.selectviewMonthlyViewButton);
+        monthlyViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showMonthlyView();
+            }
+        });
+
+        weeklyViewButton = findViewById(R.id.selectviewWeeklyViewButton);
+        weeklyViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showWeeklyView();
+            }
+        });
+
+        dailyViewButton = findViewById(R.id.selectviewDailyViewButton);
+        dailyViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDailyView();
+            }
+        });
+
+        cancelButton = findViewById(R.id.selectviewCancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCancel();
+            }
+        });
+
+        // Set Presenter
+        presenter = new SelectViewPresenter();
     }
 
     public void showMonthlyView() {
-
-    }
-
-    public void showWeeklyView() {
-
-    }
-
-    public void showDailyView() {
-
-    }
-
-    public void showCancel() {
-
-    }
-
-    public void clickMonthly(View view) {
         Intent thisIntent = new Intent(this, MonthlyCalendar.class);
         startActivity(thisIntent);
     }
 
-    public void clickWeekly(View view) {
+    public void showWeeklyView() {
         Intent thisIntent = new Intent(this, WeeklyCalendar.class);
         startActivity(thisIntent);
     }
 
-    public void clickDaily(View view) {
+    public void showDailyView() {
         Intent thisIntent = new Intent(this, DailyCalendar.class);
         startActivity(thisIntent);
     }
 
-    public void clickCancel(View view) {
+    public void showCancel() {
         finish();
     }
 }
