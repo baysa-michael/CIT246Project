28e55c1932d51d0cfc1483abf895e90741740f5a
mb1 <bay15003@byui.edu>
2019-03-16 11:57:34 -0600

    Modified activity movement logic to implement MVP for Landing Package

--- app/src/main/java/cs246/businesscalendar/view_presenter/landing/Landing.java
+++ app/src/main/java/cs246/businesscalendar/view_presenter/landing/Landing.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import cs246.businesscalendar.R;
@@ -14,11 +15,89 @@ import cs246.businesscalendar.view_presenter.select_view.SelectView;
 import cs246.businesscalendar.view_presenter.add_new.AddNew;
 
 public class Landing extends AppCompatActivity {
+    private static final String TAG = "Landing";
+    private Button scheduleButton;
+    private Button tasksButton;
+    private Button goalsButton;
+    private Button addNewButton;
+    private Button selectViewButton;
+    private Button searchButton;
+    private Button settingsButton;
+    private Button logoutButton;
+    private LandingPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        // Set Buttons
+        scheduleButton = findViewById(R.id.landingScheduleButton);
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSchedule();
+            }
+        });
+
+        tasksButton = findViewById(R.id.landingTasksButton);
+        tasksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTasks();
+            }
+        });
+
+        goalsButton = findViewById(R.id.landingGoalsButton);
+        goalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showGoals();
+            }
+        });
+
+        addNewButton = findViewById(R.id.landingAddNewButton);
+        addNewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddNew();
+            }
+        });
+
+        selectViewButton = findViewById(R.id.landingSelectViewButton);
+        selectViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSelectView();
+            }
+        });
+
+        searchButton = findViewById(R.id.landingSearchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSearch();
+            }
+        });
+
+        settingsButton = findViewById(R.id.landingSettingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSettings();
+            }
+        });
+
+        logoutButton = findViewById(R.id.landingLogoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showLogout();
+            }
+        });
+
+        // Set Presenter
+        presenter = new LandingPresenter();
 
         // Obtain Display Name in Shared Preferences
         SharedPreferences myPreferences = this.getSharedPreferences(
@@ -43,38 +122,26 @@ public class Landing extends AppCompatActivity {
     }
 
     public void showAddNew() {
-
-    }
-
-    public void showSelectView() {
-
-    }
-
-    public void showSearch() {
-
-    }
-
-    public void showSettings() {
-
-    }
-
-    public void showLogout() {
-
-    }
-
-    public void clickAddNew(View view) {
         Intent thisIntent = new Intent(this, AddNew.class);
 
         startActivity(thisIntent);
     }
 
-    public void clickSelectView(View view) {
+    public void showSelectView() {
         Intent thisIntent = new Intent(this, SelectView.class);
 
         startActivity(thisIntent);
     }
 
-    public void clickLogout(View view) {
+    public void showSearch() {
+        finish();
+    }
+
+    public void showSettings() {
+        finish();
+    }
+
+    public void showLogout() {
         finish();
     }
 }
