19b41e2f7dd4bffd3afd6106a14d265f3f1b36ed
starktj7 <starktj7@byui.edu>
2019-03-28 13:46:20 -0700

    Firebase auth. added

--- app/src/main/java/cs246/businesscalendar/view_presenter/login/Login.java
+++ app/src/main/java/cs246/businesscalendar/view_presenter/login/Login.java
@@ -10,6 +10,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.quickstart.auth.R;
+
 import cs246.businesscalendar.R;
 
 import cs246.businesscalendar.view_presenter.landing.Landing;
@@ -18,6 +32,47 @@ public class Login extends AppCompatActivity implements LoginContract.View {
     private static final String TAG = "Login";
     private LoginPresenter presenter;
 
+    private FirebaseAuth mAuth;
+
+    // Initialize Firebase Auth
+    mAuth = FirebaseAuth.getInstance();
+
+    /**
+     * Firebase checks to see if user is already logged in
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+     mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "signInWithEmail:success");
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI(user);
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+
+            // [START_EXCLUDE]
+            if (!task.isSuccessful()) {
+                mStatusTextView.setText(R.string.auth_failed);
+            }
+            hideProgressDialog();
+            // [END_EXCLUDE]
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,4 +125,7 @@ public class Login extends AppCompatActivity implements LoginContract.View {
     public void showCancel() {
         finish();
     }
+
+    //firebase sign out
+    FirebaseAuth.getInstance().signOut();
 }
